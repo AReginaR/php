49fef33a488b025f5f873d40b97242864c06225b
AReginaR <49617876+AReginaR@users.noreply.github.com>
2020-02-27 16:45:15 +0300

    dz 2

---
+++ 2/index.php
@@ -0,0 +1,39 @@
+<?php
+function generator($str, &$count){
+    for($i = 0; $i < strlen($str); $i++){
+        switch ($str[$i]){
+            case 'h':
+                $count++;
+                yield '4';
+                break;
+            case 'l':
+                $count++;
+                yield '1';
+                break;
+            case 'e':
+                $count++;
+                yield '3';
+                break;
+            case 'o':
+                $count++;
+                yield '0';
+                break;
+            default:
+                yield $str[$i];
+        }
+    }
+}
+
+function f($text){
+    $counter = 0;
+    $out = '';
+    foreach (generator($text, $counter) as $value){
+        $out .= $value;
+    }
+    echo 'line: '.$out.' count: '.$counter;
+}
+if(isset($_REQUEST['str'])){
+    $text = $_REQUEST['str'];
+    f($text);
+}
+
