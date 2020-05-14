2e98f6b91252f30160eefb55a2d9105db724b1ae
Regina19 <regina200091@gmail.com>
2020-05-14 20:38:13 +0300

    fix hw9

--- 9/index.php
+++ 9/index.php
@@ -5,29 +5,42 @@ require_once 'LoggerFile.php';
 
 if (isset($_REQUEST['submit'])){
     $text = $_REQUEST['str'];
-    $log = $_REQUEST['log'];
-    $lines = explode(PHP_EOL, $text);
-    $lines = change($lines);
 
-    if ($log == 'browser'){
-        date_default_timezone_set ( "Europe/Moscow" );
-        $date = $_REQUEST['date'];
-        if ($date == "time"){
-            $logger = new LoggerBrowser(date('H:i:s'));
-        } elseif ($date == "date_time"){
-            $logger = new LoggerBrowser(date('d m Y H:i:s'));
-        }else{
-            $logger = new LoggerBrowser('');
+    if(isset($_REQUEST['log'])){
+        $log = $_REQUEST['log'];
+
+        $lines = explode(PHP_EOL, $text);
+        $lines = change($lines);
+
+        if ($log == 'browser'){
+            date_default_timezone_set ( "Europe/Moscow" );
+            if (isset($_REQUEST['date'])) {
+                $date = $_REQUEST['date'];
+                if ($date == "time"){
+                    $logger = new LoggerBrowser(date('H:i:s'));
+                } elseif ($date == "date_time"){
+                    $logger = new LoggerBrowser(date('d m Y H:i:s'));
+                }else{
+                    $logger = new LoggerBrowser('');
+                }
+                foreach ($lines as $line) {
+                    $logger->write($line);
+                }
+            }else{
+                echo "Выберите параметр";
+            }
+        }elseif ($log == "file"){
+            if($_REQUEST['filename']){
+                $file_name = $_REQUEST['filename'];
+                $logger = new LoggerFile($file_name);
+                foreach ($lines as $line) {
+                    $logger->write($line);
+                }
+            } else {
+                echo "Введите имя файла";
+            }
         }
-        foreach ($lines as $line) {
-            $logger->write($line);
-        }
-    }elseif ($log == "file"){
-        $file_name = $_REQUEST['filename'];
-        $logger = new LoggerFile($file_name);
-        foreach ($lines as $line) {
-            $logger->write($line);
-        }
+
     } else{
         echo "выберите логгер";
     }
